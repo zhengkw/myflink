ec26f2e4c04463029a7ded1b90eed174cf790031
zhengkw <swimer@vip.qq.com>
2020-06-09 14:27:57 +0800

    commit

--- src/main/scala/com/zhengkw/day02/KeyByExample.scala
+++ src/main/scala/com/zhengkw/day02/KeyByExample.scala
@@ -1,5 +1,7 @@
 package com.zhengkw.day02
 
+import org.apache.flink.streaming.api.scala._
+
 /**
  * @ClassName:KeyByExample
  * @author: zhengkw
@@ -9,5 +11,16 @@ package com.zhengkw.day02
  * @since: jdk 1.8 scala 2.11.8
  */
 object KeyByExample {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
 
+    val stream  = env
+      .addSource(new SensorSource) // --> DataStream[T]
+      .keyBy(r => r.id) // --> KeyedStream[T, K]
+      .min(2)  // --> DataStream[T]
+
+    stream.print()
+    env.execute()
+  }
 }
