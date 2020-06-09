be2ae8233d54a0bf3d591df393ff26590e738383
zhengkw <swimer@vip.qq.com>
2020-06-09 14:37:29 +0800

    commit

---
+++ src/main/scala/com/zhengkw/day02/SplitExample.scala
@@ -0,0 +1,35 @@
+package com.zhengkw.day02
+
+import org.apache.flink.streaming.api.scala._
+
+/**
+ * @ClassName:SplitExample
+ * @author: zhengkw
+ * @description:
+ * @date: 20/06/09下午 2:37
+ * @version:1.0
+ * @since: jdk 1.8 scala 2.11.8
+ */
+object SplitExample {
+  def main(args: Array[String]): Unit = {
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
+
+    val inputStream: DataStream[(Int, String)] = env
+      .fromElements(
+        (1001, "1001"),
+        (999, "999")
+      )
+
+    val splitted: SplitStream[(Int, String)] = inputStream
+      .split(t => if (t._1 > 1000) Seq("large") else Seq("small"))
+
+    val large: DataStream[(Int, String)] = splitted.select("large")
+    val small: DataStream[(Int, String)] = splitted.select("small")
+    val all: DataStream[(Int, String)] = splitted.select("small", "large")
+
+    large.print()
+
+    env.execute()
+  }
+}
